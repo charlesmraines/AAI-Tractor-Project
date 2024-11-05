67c7775853908e2c8a51e07d9cd6f48d73d2c004
Charles Raines <charlesraines@Charless-MacBook-Pro.local>
2024-11-05 14:56:59 -0600

    Updated teleop detection

--- test_code/serial_test.py
+++ test_code/serial_test.py
@@ -1,25 +1,38 @@
 import serial
 import keyboard  # Requires installation: pip install keyboard
+import sys, termios, tty, select
 
 # Set up the serial connection (adjust 'COM3' to your Arduino's port)
 serial_port = '/dev/ttyACM0'  # Change this to your Arduino's port
 baud_rate = 9600
+settings = termios.tcgetattr(sys.stdin)
+
+# Function to get keyboard input
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    return key
 
 # Initialize serial connection
-ser = serial.Serial(serial_port, baud_rate)
+# ser = serial.Serial(serial_port, baud_rate)
+
+
 
 try:
     print("Press any key to send data to the Arduino...")
     while True:
         # Wait for a key press
-        event = keyboard.read_event()
+        # event = keyboard.read_event()
+        key = getKey()
         
         # Check if the event is a key press and if it's 'g' or 'b'
-        ser.write(event.name.encode())  # Send the command as a byte
-        print(f"Data '{event.name}' sent to Arduino.")
+        # ser.write(event.name.encode())  # Send the command as a byte
+        print(f"Data '{key}' sent to Arduino.")
         
 except KeyboardInterrupt:
     print("Program exited.")
 finally:
-    ser.close()  # Ensure the serial connection is closed
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    # ser.close()  # Ensure the serial connection is closed
 
